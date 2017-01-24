a5a64d5cea95f45e4fe1cf7ae921dc7f8c2253ef
Nick <arioxon@gmail.com>
2017-01-25 02:29:29 +0300

    Added delay for launching flower's container.

--- flower_service/run-flower.sh
+++ flower_service/run-flower.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# wait for MQ server to start
+sleep 10
 celery --loglevel=info flower --address=0.0.0.0 --port=5555 --broker_api=http://guest:guest@messagequeue:15672/api/ --broker=amqp://guest:guest@messagequeue:5672//
